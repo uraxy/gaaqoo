279edae16462618006727e18c6bf40f312164380
uraxy <uraxy123@gmail.com>
2016-09-22 00:39:33 +0900

    Config with YAML file #6

--- gaaqoo/main.py
+++ gaaqoo/main.py
@@ -7,36 +7,70 @@ import time
 import PIL.Image
 import PIL.ImageDraw
 import PIL.ImageFont
-import config
-
-EXIF_DATETIME_PARSER = re.compile(config.DATETIME_FORMAT)
-SRC_DIR = os.path.expandvars(os.path.expanduser(config.SRC_DIR))
-DST_DIR = os.path.expandvars(os.path.expanduser(config.DST_DIR))
-
-def get_hash(filepath):
-    with open(filepath, 'rb') as f:
-        data = f.read()
-    h = hashlib.sha1(data).hexdigest()
-    return h[:8]
+import yaml
 
 
-def print_exif(exif):
+def _read_config(yaml_filepath):
+    """Read config from YAML file.
+
+    Args:
+        yaml_filepath (str):
+
+    Returns:
+        dict: Config
+    """
+    with open(yaml_filepath, 'r') as f:
+        try:
+            config = yaml.load(f)
+        except yaml.YAMLError as e:  # FIXME
+            print(e)  # FIXME
+            raise e
+    config['SRC_DIR_ORG'] = config['SRC_DIR']
+    config['SRC_DIR'] = os.path.expandvars(os.path.expanduser(config['SRC_DIR']))
+    config['DST_DIR_ORG'] = config['DST_DIR']
+    config['DST_DIR'] = os.path.expandvars(os.path.expanduser(config['DST_DIR']))
+    return config
+
+
+def _print_exif(exif):
+    """Pretty print of exif.
+
+    Args:
+        exif (dict): EXIF got with _get_exif()
+    """
     for k, v in exif.items():
         print('  {} (PIL.ExifTags.TAGS[0x{:0>4x}]): {}'.format(PIL.ExifTags.TAGS[k], k, v))
 
 
-def _get_contain_size(src_img_size):
-    ratio_x = config.DST_IMG_SIZE[0] / src_img_size[0]
-    ratio_y = config.DST_IMG_SIZE[1] / src_img_size[1]
+def _get_contain_size(src_img_size, dst_img_size):
+    """Get image size which fit `dst_img_size`.
+
+    Args:
+        src_img_size (x, y):
+        dst_img_size (x, y):
+
+    Returns:
+        (x, y):
+    """
+    ratio_x = dst_img_size[0] / src_img_size[0]
+    ratio_y = dst_img_size[1] / src_img_size[1]
     if ratio_x < ratio_y:
-        size = (config.DST_IMG_SIZE[0], int(src_img_size[1] * ratio_x))
+        size = (dst_img_size[0], int(src_img_size[1] * ratio_x))
     else:
-        size = (int(src_img_size[0] * ratio_y), config.DST_IMG_SIZE[1])
+        size = (int(src_img_size[0] * ratio_y), dst_img_size[1])
 
     return size
 
 
-def get_exif(img):
+def _get_exif(img):
+    """Get EXIF from PIL.Image.
+
+    Args:
+        img (PIL.Image):
+
+    Returns:
+        dict: EXIF
+    """
     try:
         exif = img._getexif()  # AttributeError
     except AttributeError:
@@ -44,21 +78,46 @@ def get_exif(img):
     return exif
 
 
-def get_datetime_original(exif):
+def _get_datetime_original(exif):
+    """Get DateTimeOriginal from EXIF.
+
+    Args:
+        exif (dict):
+
+    Returns:
+        str: DateTimeOriginal
+    """
     if not exif:
         return None
     datetime_original = exif.get(0x9003)  # EXIF: DateTimeOriginal
     return datetime_original
 
 
-def get_orientation(exif):
+def _get_orientation(exif):
+    """Get Orientation from EXIF.
+
+    Args:
+        exif (dict):
+
+    Returns:
+        int or None: Orientation
+    """
     if not exif:
         return None
     orientation = exif.get(0x0112)  # EXIF: Orientation
     return orientation
 
 
-def transpose(src_img, exif_orientation):
+def _transpose(src_img, exif_orientation):
+    """Rotate image by EXIF Orientation.
+
+    Args:
+        src_img (PIL.Image):
+        exif_orientation (int):
+
+    Returns:
+        PIL.Image: Rotated image
+    """
     convert_image = {
         1: lambda img: img,
         2: lambda img: img.transpose(PIL.Image.FLIP_LEFT_RIGHT),
@@ -73,13 +132,28 @@ def transpose(src_img, exif_orientation):
     return dst_img
 
 
-def _overlay_text(img, text):
+def _overlay_text(
+        img,
+        text,
+        font='/usr/share/fonts/truetype/msttcorefonts/Verdana_Bold.ttf',
+        font_size=30):
+    """Overlay text on `Image`.
+
+    Args:
+        img (PIL.Image): Image, on which text is going to overlayed in place
+        text (str): text to overlay
+        font (str): font filepath
+        font_size (int): font size
+
+    Returns:
+        None:
+    """
     if not text:
         return
     draw = PIL.ImageDraw.Draw(img)
     draw.font = PIL.ImageFont.truetype(
-        font=config.FONT,
-        size=config.FONT_SIZE)
+        font=font,
+        size=font_size)
     txt_size = draw.font.getsize(text)  # (width, height)
 
     x = img.width - txt_size[0] - 5
@@ -92,26 +166,43 @@ def _overlay_text(img, text):
     draw.text((x, y), text, (255, 255, 255))
 
 
-def exif_datetime_to_text(exif_datetime):
+_EXIF_DATETIME_PARSER = re.compile(r'(\d{4}):(\d{2}):(\d{2}) (\d{2}):(\d{2}):(\d{2})')
+
+def _exif_datetime_to_text(exif_datetime):
     """ Convert EXIF style DateTime to text for overlay.
 
-    '2016:07:10 17:19:53' => '2016/07/10 17:19'
+    Args:
+        exif_datetime (str): e.g. '2016:07:10 17:19:53'
+
+    Returns:
+        string: e.g. '2016/07/10 17:19'. Seconds is ignored.
     """
-    x = EXIF_DATETIME_PARSER.match(exif_datetime)
+    x = _EXIF_DATETIME_PARSER.match(exif_datetime)
     if x:
         text = '{}/{}/{} {}:{}'.format(x.group(1), x.group(2), x.group(3), x.group(4), x.group(5))
     else:
-        text = None
+        text = ''
     return(text)
 
 
-def get_filepaths(dirpath, suffixes=None, excludes=None):
+def _get_filepaths(dirpath, suffixes=None, excludes=None):
+    """find filepaths which meet conditions (suffixes and excludes).
+
+    Args:
+        dirpath (str): Path of top directory
+        suffixes (list of str or tuple of str): Suffixes of files to get
+        excludes (list of str or tuple of str): Exclude files which contains one of this in filepath
+
+    Returns:
+        list of str: filepaths
+    """
+    suffixes = tuple(suffixes) if suffixes else ()
     if excludes is None:
-        excludes = []
+        excludes = ()
     if dirpath.endswith('/'):
         dirpath = dirpath[:-1]
-    globed = glob.glob(dirpath + '/**', recursive=True)
 
+    globed = glob.glob(dirpath + '/**', recursive=True)
     filepaths = []
     for fp in globed:
         # photo file?
@@ -121,7 +212,7 @@ def get_filepaths(dirpath, suffixes=None, excludes=None):
             continue
         # exclude?
         skip = False
-        for s in excludes:
+        for s in excludes:  # TODO must ignore leading SRC_DIR part.
             if fp.find(s) >= 0:
                 skip = True
                 break
@@ -131,29 +222,64 @@ def get_filepaths(dirpath, suffixes=None, excludes=None):
     return filepaths
 
 
-def get_dst_filepath(src_filepath, hashcode):
-    dst_fp = DST_DIR + src_filepath[len(SRC_DIR):]
+def _hash(filepath):
+    """Hash str (len=8) of file.
+
+    Args:
+        filepath (str): filepath to get hash.
+
+    Returns:
+        str: Hash
+    """
+    with open(filepath, 'rb') as f:
+        data = f.read()
+    h = hashlib.sha1(data).hexdigest()
+    return h[:8]
+
+
+def _get_dst_filepath(src_dir, dst_dir, src_filepath):
+    """Get dst filepath.
+
+    Args:
+        src_dir (str):
+        dst_DIR (str):
+        src_filepath (str):
+
+    Returns:
+        str: filepath
+    """
+    hashcode = _hash(src_filepath)
+    dst_fp = dst_dir + src_filepath[len(src_dir):]
     dst_fp += '.gaaqoo_{}.jpg'.format(hashcode)
-    # dst_fp += '.{}_{}x{}.jpg'.format(hashcode, config.DST_IMG_SIZE[0], config.DST_IMG_SIZE[1])
     return dst_fp
 
 
 def main():
-    if not os.path.isdir(SRC_DIR):
-        print('config.SRC_DIR is not a directory: {}'.format(SRC_DIR))
+    """Main."""
+    conf = _read_config('conf/default.yml')  # TODO dummy filepath
+
+    if not os.path.isdir(conf['SRC_DIR']):
+        print('SRC_DIR is not a directory: {}'.format(conf['SRC_DIR_ORG']))
         exit(1)
-    src_filepaths = get_filepaths(SRC_DIR, suffixes=config.SUFFIX, excludes=config.EXCLUDE)
+    src_filepaths = _get_filepaths(
+        conf['SRC_DIR'],
+        suffixes=conf['SUFFIX'],
+        excludes=conf['EXCLUDE'])
     if not src_filepaths:
-        print('No image file found in config.SRC_DIR: {}'.format(SRC_DIR))
+        print('No image file found in SRC_DIR: {}'.format(conf['SRC_DIR']))
         exit(1)
 
     dst_filepaths = []
     start_time = time.time()
     for i, fp in enumerate(src_filepaths):
-        print('{}/{} [{:.2f}% in {:.3f} sec] {}'.format(i+1, len(src_filepaths), i/len(src_filepaths)*100, time.time() - start_time, fp))
+        print('{}/{} [{:.2f}% in {:.3f} sec] {}'.format(
+            i+1,
+            len(src_filepaths),
+            i/len(src_filepaths)*100,
+            time.time()-start_time,
+            fp))
 
-        hashcode = get_hash(fp)
-        dst_fp = get_dst_filepath(fp, hashcode)
+        dst_fp = _get_dst_filepath(conf['SRC_DIR'], conf['DST_DIR'], fp)
         dst_filepaths.append(dst_fp)
         if os.path.isfile(dst_fp):
             print('  -> Skip, already exists: ' + dst_fp)
@@ -163,22 +289,22 @@ def main():
             img = PIL.Image.open(fp)
 
             # EXIF
-            exif = get_exif(img)
-            ori = get_orientation(exif)
+            exif = _get_exif(img)
+            ori = _get_orientation(exif)
             # print('  Orientation={}'.format(ori))
             if not ori:  # None or 0
                 ori = 1
-            dt = get_datetime_original(exif)
+            dt = _get_datetime_original(exif)
             # print('  DataTimeOriginal={}'.format(dt))
 
             # rotate: Must before resizing.
-            img = transpose(img, ori)
+            img = _transpose(img, ori)
             # resize: Not thumbnail but scale up/down to keep overlay texts at same scale.
-            dst_img_size = _get_contain_size(img.size)
+            dst_img_size = _get_contain_size(img.size, conf['DST_IMG_SIZE'])
             img = img.resize(dst_img_size, resample=PIL.Image.LANCZOS)
 
             if dt:
-                _overlay_text(img, exif_datetime_to_text(dt))
+                _overlay_text(img, _exif_datetime_to_text(dt), conf['FONT'], conf['FONT_SIZE'])
 
             # save image
             d = dst_fp.rpartition('/')
@@ -196,7 +322,7 @@ def main():
                 pass
 
     # delete dst-file which have no src-file
-    dst_filepaths_exists = get_filepaths(DST_DIR)
+    dst_filepaths_exists = _get_filepaths(conf['DST_DIR'])
     for fp in dst_filepaths_exists:
         if fp not in dst_filepaths:
             print('Removing deprecated file: ' + fp)
